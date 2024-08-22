fbf0c67c8026be0cb1ed1de1fcf9f57041cd4a50
Adarsh kumar <adarshkumar0128@gmail.com>
2024-08-23 01:09:18 +0530

    6

--- BackendServices/index.js
+++ BackendServices/index.js
@@ -2,10 +2,24 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const dbConnection = require("./utils/db");
+const cron = require("node-cron");
 dotenv.config();
 
 // SERVER
 const PORT = process.env.PORT;
+
+// SCHEDULE TASK
+
+const run = () => {
+  cron.schedule("* * * * * *", () => {
+    sendDetailsProspectEmail();
+    sendEligibilityEmail();
+    sendBloodDonationReminder();
+    sendDonorDetailsEmail();
+  });
+};
+
+run();
 
 app.listen(8001, () => {
   console.log(`Backgroundservices is running on 8001`);

3ae9e1a9c711c53dda12f4004386e86af99ef0c3
Adarsh kumar <adarshkumar0128@gmail.com>
2024-08-23 00:35:19 +0530

    5

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const authRoute = require("./routes/auth");
+const donorRoute = require("./routes/donor");
+const prospectRoute = require("./routes/prospect");
 module.exports = app;
 
 //cors
@@ -12,3 +14,5 @@ app.use(express.json());
 
 //routes
 app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/donors", donorRoute);
+app.use("/api/v1/prospects", prospectRoute);

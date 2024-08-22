a4c177faefdcd3a46f3fa2da6e1a932da022e9f9
Adarsh kumar <adarshkumar0128@gmail.com>
2024-08-23 00:22:15 +0530

    second commit --4

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
+const authRoute = require("./routes/auth");
 module.exports = app;
+
+//cors
+app.use(cors());
+
+//json
+app.use(express.json());
+
+//routes
+app.use("/api/v1/auth", authRoute);
